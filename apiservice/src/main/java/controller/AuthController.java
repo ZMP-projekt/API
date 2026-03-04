d1d43b8b3f3b42bd2c339053c2a525a0b5942b5f
Roksana Żyłka <roksana.zylka@studenci.collegiumwitelona.pl>
2026-03-04 20:34:08 +0100

    endpoint do wylogowania
    bo zapomnialam

--- apiservice/src/main/java/controller/AuthController.java
+++ apiservice/src/main/java/controller/AuthController.java
@@ -1,14 +1,23 @@
 package controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import dto.AuthResponse;
 import dto.LoginRequest;
 import dto.RegisterRequest;
-import service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import service.AuthService;
 
 @RestController
 @RequestMapping("/auth")
@@ -29,4 +38,14 @@ public class AuthController {
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
+
+    @Operation(summary = "Wylogowanie", description = "Inwaliduje sesję użytkownika")
+    @PostMapping("/logout")
+public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth != null) {
+        new SecurityContextLogoutHandler().logout(request, response, auth);
+    }
+    return ResponseEntity.ok("Pomyślnie wylogowano.");
+}
 }
